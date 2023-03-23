1e31f072a4ad9583099008899f4c54e1eba2ccfa
Tatiana Noskova <tnoskowa@mail.ru>
2023-03-23 19:01:28 +0500

    test fix unregistered

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -40,8 +40,8 @@ public class BonusServiceTest {
 
         // подготавливаем данные:
         long amount = 1000;
-        boolean registered = true;
-        long expected = 30;
+        boolean registered = false;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -56,7 +56,7 @@ public class BonusServiceTest {
 
         // подготавливаем данные:
         long amount = 1_000_000;
-        boolean registered = true;
+        boolean registered = false;
         long expected = 500;
 
         // вызываем целевой метод:
